4d79f48e86bf1eca9869ef222eaccce011b89032
Sanket-Sasane <sanketsasane07@gmail.com>
2024-04-17 18:04:16 +0530

    react app created and do basic progam

---
+++ 01basicreact/src/App.js
@@ -0,0 +1,36 @@
+
+function App() {
+  return (
+    <div>
+      <h1>Basic React App | Sanket Sasane - The basic react App </h1>
+
+      <table>
+        <tr>
+          <th>Sr.No</th>
+          <th>Name</th>
+          <th>Education</th>
+        </tr>
+
+        <tr>
+          <td>1.</td>
+          <td>Samrat</td>
+          <td>BSc</td>
+        </tr>
+
+        <tr>
+          <td>2.</td>
+          <td>Sayali</td>
+          <td>BCOM</td>
+        </tr>
+
+        <tr>
+          <td>3.</td>
+          <td>Suraj</td>
+          <td>BA</td>
+        </tr>
+      </table>
+    </div>
+  );
+}
+
+export default App;
